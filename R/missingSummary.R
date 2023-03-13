d546af0e62c18d75c86939b5a7454a3c6767834a
Benjamin <benjamin.nutter@gmail.com>
2023-03-13 14:43:37 -0400

    Revert "Tests for missingSummary"
    This reverts commit e48637f2bad56cf113206520ca360e42abd06029.

--- R/missingSummary.R
+++ R/missingSummary.R
@@ -102,9 +102,6 @@ missingSummary.redcapApiConnection <- function(rcon,
                                len = 1, 
                                add = coll)
   
-  checkmate::assert_character(x = fixed_fields, 
-                              add = coll)
-  
   error_handling <- checkmate::matchArg(x = error_handling,
                                         choices = c("null", "error"),
                                         add = coll)
