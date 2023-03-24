686fbf082ea61d721bd1282e0879a19f1012a79e
Shawn Garbett <shawn@garbett.org>
2023-03-24 09:44:25 -0500

    Fixed test breakage from exportRecords message

--- tests/testthat/test-exportRecords.R
+++ tests/testthat/test-exportRecords.R
@@ -3,7 +3,7 @@ context("exportRecords")
 rcon <- redcapConnection(url = url, token = API_KEY)
 
 test_that("records can be exported",{
-  expect_silent(rec <- exportRecords(rcon))
+  expect_message(rec <- exportRecords(rcon))
   expect_gte(length(rec), 1)
 })
 
