5086d1687b2240bb7ffe0459a1544200fa2152dd
Shawn Garbett <shawn@garbett.org>
2023-04-07 11:31:36 -0500

    Expanding tests for #10

--- tests/testthat/test-exportRecordsTyped.R
+++ tests/testthat/test-exportRecordsTyped.R
@@ -83,12 +83,31 @@ test_that(
 )
 
 test_that(
+  "Return an error if drop_fields is not character",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    drop_fields = 1:2),
+                 "'drop_fields'[:] Must be of type 'character'")
+  }
+)
+
+test_that(
   "Return an error if forms is not character",
   {
     local_reproducible_output(width = 200)
     expect_error(exportRecordsTyped(rcon,
                                fields = 1:2),
                  "'fields'[:] Must be of type 'character'")
+  }
+)
+
+test_that(
+  "Return an error if events is not character",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon, events = 1),
+                 "'events'[:] Must be of type 'character'")
   }
 )
 
@@ -103,15 +122,6 @@ test_that(
                      WithNumeric)
     expect_error(exportRecordsTyped(rcon, records = TRUE),
                  "'records'[:] Must be of type 'character'")
-  }
-)
-
-test_that(
-  "Return an error if events is not character",
-  {
-    local_reproducible_output(width = 200)
-    expect_error(exportRecordsTyped(rcon, events = 1),
-                 "'events'[:] Must be of type 'character'")
   }
 )
 
@@ -142,14 +152,140 @@ test_that(
 )
 
 test_that(
-  "Return an error if batch.size is not integerish(1)",
+  "Return an error if date_begin is not POSIXct",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    date_begin=TRUE),
+                 "'date_begin'[:] Must be of type 'POSIXct'")
+  }
+)
+
+test_that(
+  "Return an error if date_end is not POSIXct",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    date_end=FALSE),
+                 "'date_end'[:] Must be of type 'POSIXct'")
+  }
+)
+
+
+test_that(
+  "Return an error if batch_size is not integerish(1)",
   {
     local_reproducible_output(width = 200)
     expect_error(exportRecordsTyped(rcon,
                                     batch_size = c(-1, 100)),
-                 "'batch_size': Must have length <= 1, but has length 2.")
+                 "'batch_size'[:] Must have length <= 1, but has length 2.")
     expect_error(exportRecordsTyped(rcon,
                                     batch_size = "-1"),
                  "'batch_size'[:] Must be of type 'integerish'")
+  }
+)
+
+test_that(
+  "Return an error if csv_delimiter is not an allowed character",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    csv_delimiter="*"),
+                 "'csv_delimiter'[:] Must be element of set")
+    expect_error(exportRecordsTyped(rcon,
+                                    csv_delimiter=",,"),
+                 "'csv_delimiter'[:] Must be element of set")
+  }
+)
+
+test_that(
+  "Return an error if config is not a list",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    config=1:3),
+                 "'config'[:] Must be of type 'list'")
+    expect_error(exportRecordsTyped(rcon,
+                                    config=list("a")),
+                 "'config'[:] Must have names")
+  }
+)
+
+test_that(
+  "Return an error if api_param is not a list",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    api_param=1:3),
+                 "'api_param'[:] Must be of type 'list'")
+    expect_error(exportRecordsTyped(rcon,
+                                    api_param=list("a")),
+                 "'api_param'[:] Must have names")
+  }
+)
+
+test_that(
+  "Return an error if na is not a list",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    na=1:3),
+                 "'na'[:] Must be of type 'list'")
+    expect_error(exportRecordsTyped(rcon,
+                                    na=list("a")),
+                 "'na'[:] Must have names")
+  }
+)
+
+test_that(
+  "Return an error if validation is not a list",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    validation=1:3),
+                 "'validation'[:] Must be of type 'list'")
+    expect_error(exportRecordsTyped(rcon,
+                                    validation=list("a")),
+                 "'validation'[:] Must have names")
+  }
+)
+
+test_that(
+  "Return an error if cast is not a list",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    cast=1:3),
+                 "'cast'[:] Must be of type 'list'")
+    expect_error(exportRecordsTyped(rcon,
+                                    cast=list("a")),
+                 "'cast'[:] Must have names")
+  }
+)
+
+test_that(
+  "Return an error if assignment is not a list",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    assignment=1:3),
+                 "'assignment'[:] Must be of type 'list'")
+    expect_error(exportRecordsTyped(rcon,
+                                    assignment=list("a")),
+                 "'assignment'[:] Must have names")
+  }
+)
+
+
+test_that(
+  "Return an error if mChoice is not logical(1)",
+  {
+    local_reproducible_output(width = 200)
+    expect_error(exportRecordsTyped(rcon,
+                                    mChoice = c(TRUE, FALSE)),
+                 "'mChoice'[:] Must have length 1")
+    expect_error(exportRecordsTyped(rcon,
+                                    mChoice = "TRUE"),
+                 "'mChoice'[:] Must be of type 'logical'")
   }
 )
