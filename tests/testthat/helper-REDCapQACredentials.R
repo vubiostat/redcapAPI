67aedbda71c79118640b0294a40753902f0ef1c2
Benjamin <benjamin.nutter@gmail.com>
2023-04-07 15:33:45 -0400

    Begin reorganizing tests

--- tests/testthat/helper-REDCapQACredentials.R
+++ tests/testthat/helper-REDCapQACredentials.R
@@ -14,7 +14,9 @@
 # 
 #   options(keyring_backend=keyring::backend_file) # Because MACOS is so irritating
 #   keyring::keyring_create('TestRedcapAPI', 'YOURPASSWORDHERE')
+#   keyring::keyring_create('SandboxAPI', 'YOURPASSWORDHERE)
 #   keyring::key_set_with_value('TestRedcapAPI', username='TestRedcapAPI', keyring='TestRedcapAPI', password='YOURAPIKEYHERE')
+#   keyring::key_set_with_value('SandboxAPI', username='SandboxAPI', keyring='SandboxAPI', password='SandboxAPIKEYHERE')
 # To remove invalid password/API_KEY
 #   keyring::key_delete('TestRedcapAPI', 'TestRedcapAPI', 'TestRedcapAPI')
 
@@ -26,6 +28,11 @@ if(!exists("password")){
 if(!exists("API_KEY")){
   keyring::keyring_unlock('TestRedcapAPI', password)
   API_KEY <- keyring::key_get('TestRedcapAPI', 'TestRedcapAPI', 'TestRedcapAPI')
+}
+
+if(!exists("SANDBOX_API_KEY")){
+  keyring::keyring_unlock('SandboxAPI', password)
+  SANDBOX_KEY <- keyring::key_get('SandboxAPI', 'SandboxAPI', 'SandboxAPI')
 }
 
 library(checkmate) # for additional expect_* functions.
---
+++ tests/testthat/test-01-arms-functionality.R
@@ -0,0 +1,1 @@
+context("export/import/delete Arms Functionality")
