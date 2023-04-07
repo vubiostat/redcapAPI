08451c68c011ebabf615ee75ac1273d93702b3ac
Benjamin <benjamin.nutter@gmail.com>
2023-04-07 07:33:57 -0400

    A couple more suggestions

--- R/exportRecordsTyped.R
+++ R/exportRecordsTyped.R
@@ -25,7 +25,8 @@
 #   * Documentation cleanup
 #   * Add actual functions to do work
 # [EXTERNALIZED CONCERN-NOT DONE HERE]Handle retries--with batched backoff(?)
-# Offline version? testing?
+# Offline version? testing? [This could be deferred. A lot of the code could be reused if we can
+#                            identify pieces to turn into subroutines.]
 # Cleanup / review / editing pass
 # Test cases (If we put in broken data, this will break existing method). Thus get the existing tests working with new method and expect the old one to break.
 # [DONE] Figure out the mChoice strategy (dealing with an out of defined scope request from a power user).
@@ -157,7 +158,9 @@ castCheckCode  <- function(x, coding, field_name) factor(c("", gsub(".*___(.*)",
 stripHTMLandUNICODE <- function(field_name, field_label, field_annotation)
 {
   # FIXME FIXME
-  field_label
+  removeUnicode(
+    removeHtmlTags(field_label)
+  )
 }
 #' @rdname unitsFieldAnnotation
 #' @export
@@ -801,34 +804,7 @@ exportRecordsTyped.redcapApiConnection <-
                      records_raw = Raw, 
                      checkbox_fieldname = checkbox_fields[i], 
                      style = "labelled")
-
-  # simply not requested? I.e. meta data versus retrieved mismatch
-  if(mChoice)
-  {
-    checkbox_meta <- meta_data[which(meta_data$field_type == 'checkbox'),]
-    for(i in seq_len(nrow(checkbox_meta)))
-    {
-      checkbox_fieldname <- checkbox_meta$field_name[i]
-      fields <- recordnames[grepl(sprintf("^%s", checkbox_fieldname), recordnames)]
-      if(length(fields) > 0)
-      {
-        # FIXME: Issue-38 when merged will provide this as a function
-        # I.e. is this fieldChoiceMapping?
-        opts   <- strsplit(strsplit(checkbox_meta[i,'select_choices_or_calculations'],"\\s*\\|\\s*")[[1]],
-                           "\\s*,\\s*")
-        levels <- sapply(opts, function(x) x[1+labels])
-        # END FIXME
-        opts <- as.data.frame(matrix(rep(seq_along(fields), nrow(records)), nrow=nrow(records), byrow=TRUE))
-        checked <- records_raw[,fields] != '1'
-        opts[which(checked,arr.ind=TRUE)] <- ""
-        z <- structure(
-          gsub(";$|^;", "",gsub(";{2,}",";", do.call('paste', c(opts, sep=";")))),
-          label  = checkbox_fieldname,
-          levels = levels,
-          class  = c("mChoice", "labelled"))
-  
-        records[[checkbox_fieldname]] <- z
-      }
+      
     }
   } # mChoice 
   
@@ -1082,4 +1058,82 @@ mChoiceField <- function(rcon, records_raw, checkbox_fieldname, style = c("coded
   Batched <- do.call("rbind", Batched)
   rownames(Batched) <- NULL
   Batched
+}
+
+#' @name stringCleanup
+#' @title Remove Undesired Characters From Strings
+#' 
+#' @description These functions are utilities to clear undesired characters
+#' from REDCap output.
+#' 
+#' @param x \code{character}, vector of content to be cleaned.
+#' @param tags \code{character}, vector of HTML tags to remove from \code{x}
+#' @param ignore.case \code{logical(1)}, should cases be ignored when matching
+#'   patterns? Defaults to \code{TRUE}.
+#'   
+#' @export
+
+
+removeHtmlTags <- function(x, 
+                           tags = c("p", "br", "div", "span", "b", "font", "sup", "sub"), 
+                           ignore.case = TRUE){
+  ###################################################################
+  # Argument Validation
+  
+  coll <- checkmate::makeAssertCollection()
+  
+  checkmate::assert_character(x = x, 
+                              add = coll)
+  
+  checkmate::assert_character(x = tags, 
+                              add = coll)
+  
+  checkmate::assert_logical(x = ignore.case, 
+                            len = 1, 
+                            any.missing = FALSE, 
+                            add = coll)
+  
+  checkmate::reportAssertions(coll)
+  
+  ###################################################################
+  # Regex explanation 
+  # < : match the opening of the tag
+  # ([/]|) : The pipe (|) is an or operator. So this is a match of either nothing or /
+  #          This makes sure that both <p> and </p> tags are matched, for example
+  # (%s) : filled with the tags argument, but collapsed to (p|br|...). This is
+  #        matching the tags listed in the tags argument
+  # (|.+) : matches either nothing following the tag identifier, or any number of characters
+  #         until it reaches the closing >
+  # *? : Make the match 'non-greedy', that is, it will start the search at < and stop
+  #      at the first > it encounters.
+  regex <- sprintf("<([/]|)(%s)(|.+)*?>", 
+                   paste0(tags, 
+                          collapse = "|"))
+  x <- trimws(gsub(regex, "", x, ignore.case = ignore.case))
+  x <- gsub("\\n", "", x)
+  x
+}
+
+#' @rdname stringCleanup
+#' @export
+
+removeUnicode <- function(x){
+  ###################################################################
+  # Argument Validation
+  
+  coll <- checkmate::makeAssertCollection()
+  
+  checkmate::assert_character(x = x, 
+                              add = coll)
+  
+  checkmate::reportAssertions(coll)
+  
+  ###################################################################
+  # Regex explanation
+  # See: https://stackoverflow.com/questions/39993715/how-to-remove-unicode-u00a6-from-string
+  # <U\\+ - a literal char sequence <U+
+  # \\w+ - 1 or more letters, digits or underscores
+  # > - a literal >
+  
+  gsub("<(U}u)\\+\\w+>", "", x)
 }
