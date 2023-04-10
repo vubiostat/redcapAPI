9848c9b033b77d5e590df17e26be69b16d9ab056
Shawn Garbett <shawn@garbett.org>
2023-04-10 12:29:19 -0500

    Fully working version of #35

--- R/invalid.R
+++ R/invalid.R
@@ -1,4 +1,22 @@
-
+#' @rdname invalidSummary
+#' @export
+#' 
+#' @title Helper functions for formatting validation failure report
+#' @description \code{\link{exportRecordsTyped}} may have an \code{invalid}
+#' attribute if validations fail. This data has some routines which
+#' help locate the failing records.
+#'
+#' @param x The `invalid` object
+#' @param ... additional arguments to `print`
+#' 
+#' #' @examples
+#' \dontrun{
+#' rcon <- redcapConnection(url=[YOUR_REDCAP_URL], token=[API_TOKEN])
+#' 
+#' rec <- exportRecordsTyped(rcon)
+#' 
+#' attr(rec, "invalid")
+#' }
 format.invalid <- function(x, ...)
 {
   dt <- attr(x, "time")
@@ -22,11 +40,15 @@ format.invalid <- function(x, ...)
   )
 }
 
+#' @rdname invalidSummary
+#' @export
 print.invalid <- function(x, ...)
 {
   cat(format(x), ...)
 }
 
+#' @rdname invalidSummary
+#' @export
 summary.invalid <- function(x, ...)
 {
   paste0("There are ", nrow(x), " validation failures over the following fields: '",
