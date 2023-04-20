aa531839c47908fce42c8c9ad975d02b620ba3b1
Benjamin <benjamin.nutter@gmail.com>
2023-04-20 08:33:05 -0400

    exportRepeatingInstrumentsEvents and

--- tests/testthat/test-12-repeatInstEvent-functionality.R
+++ tests/testthat/test-12-repeatInstEvent-functionality.R
@@ -1,1 +1,15 @@
-# FIXME: Add tests after writing functions
+context("Export and Import Repeating Instruments and Events Functionality")
+
+#####################################################################
+# exportRepeatingInstrumentsEvents                               ####
+
+test_that(
+  {
+    local_reproducible_output(width = 200)
+    skip_if(!STRUCTURAL_TEST_READY, 
+            "Infrastructure not quite ready for structural tests.")
+    
+    expect_data_frame(exportRepeatingInstrumentsEvents(rcon), 
+                      ncols = 3)
+  }
+)
