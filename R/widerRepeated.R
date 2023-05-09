f69df5152ab824998b5338918e765a95cab4e5ce
Obregon, Savannah <savannah.obregon@vumc.org>
2023-05-09 16:02:23 -0500

    assume input is in expected formt, if redcap_instrument present return reshape, else return input

--- R/widerRepeated.R
+++ R/widerRepeated.R
@@ -1,17 +1,18 @@
 #' @name widerRepeated
 #' @title Transform Data Into Wide Format
 #' 
-#' @description Converts a dataframe into wide format given the column names to expand.
+#' @description Converts a dataframe into wide format given a single REDCap form.
+#' This function assumes that the Records argument is the result of exportRecordsTyped,
+#' and that all empty values have been previously dropped.
 #'   
 #' @param Records A \code{data.frame} containing the records from
 #'        \code{\link{exportRecordsTyped}}
 #' @param rcon A REDCap connection object as created by \code{unlockREDCap}.
-#' @param idvar \code{character}, Names of one or more variables in long format that identify multiple records belonging to the same group. 
-#'        These variables may also be present in wide format. (e.g., 'record_id')
 #' @export
-widerRepeated <- function(Records, rcon, idvar)
+widerRepeated <- function(Records, rcon)
 {
-   ###########################################################################
+  idvar <- rcon$metadata()$field_name[1]
+  ###########################################################################
   # Check parameters passed to function
   coll <- checkmate::makeAssertCollection()
   
@@ -25,87 +26,25 @@ widerRepeated <- function(Records, rcon, idvar)
   checkmate::assert_subset(x = idvar, names(Records))
   checkmate::reportAssertions(coll)
   
-  # set everything as character in data and data dictionary and assign id.temp to data
-  dat <- Records #data.frame(lapply(Records, as.character), stringsAsFactors = FALSE)
-  names(dat)[names(dat) == idvar] <- "id.tmp"
-  
-  dd <- data.frame(lapply(rcon$metadata(), as.character), stringsAsFactors = FALSE)
-  
-  # get correct event mappings if redcap_event_name is present or not
-  if ("redcap_event_name" %in% names(dat)) {
-    #get the event-form mapping
-    event.map <- rcon$mapping()
+  # check that redcap_repeat_instrument is present and that all values are the same
+  if ("redcap_repeat_instrument" %in% colnames(Records) && all(!is.na(Records$redcap_repeat_instrument))) {
+    if (length(unique(Records$redcap_repeat_instrument)) == 1) {
+      cat("Great! The redcap_repeat_instrument column has unique values.\n")
+      
+      Records_wide <- reshape(
+        data = Records,
+        idvar = c(idvar, "redcap_event_name", "redcap_repeat_instrument"),
+        timevar = "redcap_repeat_instance",
+        direction = "wide"
+      )
+      return(Records_wide)
+      
+    } else {
+      stop("The redcap_repeat_instrument column does not have unique values.\n")
+    }
   } else {
-    event.map = data.frame(
-      unique_event_name = "",
-      form = rcon$instruments()$instrument_name)
+    cat("The redcap_repeat_instrument column is not present.\n")
+    return(Records)
   }
-  
-  # because not all projects will have all id modifier fields
-  df.all_ids <- data.frame(id.tmp = character(0), redcap_event_name = character(0), redcap_repeat_instrument = character(0), redcap_repeat_instance = character(0))
-  tmpd <- merge(
-    data.frame(dat),
-    df.all_ids,
-    by = names(df.all_ids)[
-      names(df.all_ids) %in% names(dat)],
-    all = TRUE)
-  
-  id.fields = names(df.all_ids)
-  
-  map <- event.map
-  # data.frame(map) # This appears to do nothing
-  map[is.na(map)] <- ""
-  
-  for (i in sort(unique(dd$form_name)))
-  {
-    vars.tmp <- names(dat)[sub(REGEX_CHECKBOX_FIELD_NAME, "\\1", names(dat)) %in% dd$field_name[dd$form_name == i]]
-    
-    # form with descriptive only
-    if (length(vars.tmp) == 0) next
-    
-    tmp <- tmpd[ tmpd$redcap_event_name %in% map[map$form == i, "unique_event_name"] &
-                 (tmpd$redcap_repeat_instrument == i | isNAorBlank(tmpd$redcap_repeat_instrument)),
-                c(id.fields, vars.tmp) ]
-    
-    for(j in id.fields) tmp[is.na(tmp[,j]),j] <- ""
-
-    tmp <- reshape(tmp, varying = list(vars.tmp), times = vars.tmp, timevar = 'variable', v.names = 'value', idvar = id.fields, direction = "long")
-    tmp <- tmp[!isNAorBlank(tmp$value),]
-    
-    if (nrow(tmp) > 0) {
-      
-      cat(i, "\n")
-      # Convert tmp to wide format
-      tmp <- reshape(tmp, idvar = id.fields, direction = "wide", timevar = 'variable', v.names = 'value', varying = vars.tmp) # reshape(tmp, idvar = c(id.fields), direction = "wide")
-      # drop redcap_repeat_instrument column
-      tmp$redcap_repeat_instrument <- NULL
-      # add form column
-      tmp$name.form <- i
-      rownames(tmp) <- NULL
-      
-    } else {
-      tmp <- data.frame(matrix(NA, 0, length(c(id.fields, vars.tmp))))
-      names(tmp) = c(id.fields, vars.tmp)
-    }
-    
-    what.has <- with(
-      tmp,
-      c(all(isNAorBlank(redcap_event_name)),
-        all(isNAorBlank(redcap_repeat_instance))))
-    if (what.has[[1]] == TRUE) tmp$redcap_event_name <- NULL
-    if (what.has[[2]] == TRUE) tmp$redcap_repeat_instance <- NULL
-    
-    tmp2 <- list(tmp)
-    names(tmp2) <- i
-    
-    if (exists("list.forms")) {
-      list.forms = c(tmp2, list.forms)
-    } else {
-      list.forms = tmp2
-    }
-    rm(i, tmp)
-  }
-  # return(list.forms)
-  return(if(length(list.forms) == 1) list.forms[[1]] else list.forms)
 }
 
