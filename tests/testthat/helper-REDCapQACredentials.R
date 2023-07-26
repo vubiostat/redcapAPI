52914498d5e65c7e68a741da4071be0acd2d96fb
Shawn Garbett <shawn@garbett.org>
2023-07-26 13:30:53 -0500

    Minor bump to test helper to work on cloud platforms. Version bump in README

--- tests/testthat/helper-REDCapQACredentials.R
+++ tests/testthat/helper-REDCapQACredentials.R
@@ -15,8 +15,12 @@
 # It will name it the service "redcapAPI"
 # It will ask to save an API_KEY in this ring (there can be multiple!)
 #   of the name "TestRedcapAPI"
+  
+library(checkmate) # for additional expect_* functions.
+library(keyring)
+  
 url <- "https://redcap.vanderbilt.edu/api/" # Our institutions REDCap instance
 
 unlockREDCap(c(rcon="TestRedcapAPI"), url=url, keyring='API_KEYs', envir=globalenv())
 
-library(checkmate) # for additional expect_* functions.
+
