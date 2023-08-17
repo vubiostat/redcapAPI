afd306adc86f9fe2a18c181120bedb50e02889b4
Benjamin <benjamin.nutter@gmail.com>
2023-08-17 15:50:40 -0400

    more exportRecords tests

--- tests/testthat/test-152-exportRecordsWithDags.R
+++ tests/testthat/test-152-exportRecordsWithDags.R
@@ -1,8 +1,47 @@
 context("Export Records Functionality with Data Access Groups")
 
+# NOTE: Data for these tests was established in 
+#       test-200-exportTypedRecords-Functionality.R
+
+ImportData <- exportRecordsTyped(rcon, 
+                                 cast = raw_cast)
+ImportData <- castForImport(ImportData, 
+                            rcon)
+
+#####################################################################
+# Create DAGs to use in testing                                  ####
+
+importDags(rcon, 
+           data = data.frame(data_access_group_name = c("Test DAG 1", 
+                                                        "Test DAG 2"), 
+                             unique_group_name = rep(NA_character_, 2)))
+
+ImportData$redcap_data_access_group <- rep(rcon$dags()$unique_group_name, 
+                                           length.out = nrow(ImportData))
+
+importRecords(rcon, ImportData)
+
+#####################################################################
+# Export Data Access Groups
+
 test_that(
-  "Functionality", 
+  "DAGs can be labelled or raw", 
   {
-    skip("Tests to be developed for 3.0.0 release")
+    Dags <- exportRecords(rcon, 
+                          export_dags = TRUE)
+    expect_true(all(Dags$redcap_data_access_group %in% c("test_dag_1", "test_dag_2")))
+    
+    DagsLabel <- exportRecords(rcon, 
+                            export_dags = TRUE, 
+                            raw_or_label = "label")  
+    expect_true(all(DagsLabel$redcap_data_access_group %in% c("Test DAG 1", "Test DAG 2")))
+  }
+)
+
+test_that(
+  "DAGs can be excluded", 
+  {
+    Rec <- exportRecords(rcon, export_dags = FALSE)
+    expect_false("redcap_data_access_group" %in% names(Rec))
   }
 )
--- tests/testthat/test-155-exportRecordsWithRepeatingInstruments.R
+++ tests/testthat/test-155-exportRecordsWithRepeatingInstruments.R
@@ -1,8 +1,63 @@
 context("Export Records Functionality with Repeating Instruments")
 
+#####################################################################
+# Prepare data with Repeating Instruments                        ####
+
+load(file.path(test_path("testdata"),
+               "test_redcapAPI_MetaData.Rdata"))
+load(file.path(test_path("testdata"), 
+               "test_redcapAPI_Data.Rdata"))
+
+importMetaData(rcon, 
+               test_redcapAPI_MetaData)
+
+rcon$refresh_instruments()
+rcon$refresh_fieldnames()
+
+forms <- rcon$instruments()$instrument_name
+Mappings <- data.frame(arm_num = rep(1, length(forms)), 
+                       unique_event_name = rep("event_1_arm_1", length(forms)), 
+                       form = forms)
+importMappings(rcon, 
+               data = Mappings)
+
+RepeatInst <- data.frame(event_name = "event_1_arm_1", 
+                         form_name = "repeating_instrument")
+
+importRepeatingInstrumentsEvents(rcon, 
+                                 data = RepeatInst)
+
+ImportData <- castForImport(test_redcapAPI_Data, 
+                            rcon)
+
+importRecords(rcon, ImportData)
+
+#######################################################################
+# Export Records with Repeating Instruments                        ####
+
 test_that(
-  "Functionality", 
+  "Exported data has repeating instruments", 
   {
-    skip("Tests to be developed for 3.0.0 release")
+    Rec <- exportRecords(rcon)
+    expect_true(any(!is.na(Rec$redcap_repeat_instrument)))
+    expect_true(any(!is.na(Rec$redcap_repeat_instrument)))
+  }
+)
+
+test_that(
+  "Repeating Instrument name can be labelled or raw", 
+  {
+    Rec <- exportRecords(rcon)
+    
+    expect_true(all(Rec$redcap_repeat_instrument %in% 
+                      c(NA, rcon$instruments()$instrument_name)))
+    
+    
+    
+    Rec <- exportRecordsTyped(rcon, 
+                              raw_or_label = "label")
+    
+    expect_true(all(Rec$redcap_repeat_instrument %in% 
+                      c(NA, rcon$instruments()$instrument_label)))
   }
 )
