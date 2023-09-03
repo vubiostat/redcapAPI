cb2fd85ea0d2033d028945bc1a3b7ef99473f040
Benjamin <benjamin.nutter@gmail.com>
2023-09-03 08:06:55 -0400

    user methods documentation

--- R/exportRepeatingInstrumentsEvents.R
+++ R/exportRepeatingInstrumentsEvents.R
@@ -1,27 +1,5 @@
-#' @name exportRepeatingInstrumentsEvents
-#' @title Export Repeating Instruments and Events Settings
-#' 
-#' @description This method allows you to export a list of the repeated 
-#'   instruments and repeating events for a project. This includes 
-#'   their unique instrument name as seen in the second column of the 
-#'   Data Dictionary, as well as each repeating instrument's corresponding 
-#'   custom repeating instrument label. For longitudinal projects, the 
-#'   unique event name is also returned for each repeating instrument. 
-#'   Additionally, repeating events are returned as separate items, in 
-#'   which the instrument name will be blank/null to indicate that it is a 
-#'   repeating event (rather than a repeating instrument).
-#'   
-#' @param rcon A \code{redcapConnection} object. 
-#' @param ... Arguments to pass to other methodsArguments to be passed to other methods.
-#' @param error_handling An option for how to handle errors returned by the API.
-#'   see \code{\link{redcapError}}
-#' @param config \code{list} Additional configuration parameters to pass to 
-#'   \code{\link[httr]{POST}}. These are appended to any parameters in 
-#'   \code{rcon$config}.
-#' @param api_param \code{list} Additional API parameters to pass into the
-#'   body of the API call. This provides users to execute calls with options
-#'   that may not otherwise be supported by \code{redcapAPI}.
-#' 
+#' @describeIn repeatingInstrumentMethods Export repeating instruments and events.
+#' @order 1
 #' @export
 
 exportRepeatingInstrumentsEvents <- function(rcon, 
@@ -29,7 +7,8 @@ exportRepeatingInstrumentsEvents <- function(rcon,
   UseMethod("exportRepeatingInstrumentsEvents")
 }
 
-#' @rdname exportRepeatingInstrumentsEvents
+#' @rdname repeatingInstrumentMethods
+#' @order 3
 #' @export
 
 exportRepeatingInstrumentsEvents.redcapApiConnection <- function(rcon, 
