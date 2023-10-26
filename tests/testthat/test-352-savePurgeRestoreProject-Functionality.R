48b9fceab1d61b420c9725f037c1378225e916e1
Benjamin <benjamin.nutter@gmail.com>
2023-10-25 21:43:37 -0400

    extending preserve project

--- tests/testthat/test-352-savePurgeRestoreProject-Functionality.R
+++ tests/testthat/test-352-savePurgeRestoreProject-Functionality.R
@@ -1,7 +1,7 @@
 context("Preserve, Purge, and Restore Projects Functionality")
 
 #####################################################################
-# Preserve, Purge,  and Restore a Project                        ####
+# preserveProject                                                ####
 
 test_that(
   "Preserve a project", 
@@ -27,23 +27,79 @@ test_that(
   }
 )
 
-# test_that(
-#   "Purge and restore a project from data frames",
-#   {
-#     expect_no_error(purgeProject(rcon, records = TRUE))
-# 
-#     expect_no_error(restoreProject(rcon,
-#                                    project_information = rcon$project_information(),
-#                                    meta_data = rcon$meta_data()))
-#   }
-# )
+test_that(
+  "Preserve a project to an Rdata file", 
+  {
+    temp_dir <- tempdir()
+    rdata_file <- file.path(temp_dir, 
+                            sprintf("project-%s-RedcapList.Rdata", 
+                                    rcon$projectInformation()$project_id))
+    
+    expect_true(preserveProject(rcon, 
+                                save_as = "Rdata", 
+                                dir = temp_dir, 
+                                dir_create = TRUE))
+    
+    load(rdata_file)
+    
+    expect_no_error({
+      suppressWarnings({
+        off_con <- 
+          offlineConnection(meta_data = RedcapList$meta_data, 
+                            arms = RedcapList$arms, 
+                            events = RedcapList$events,  
+                            mapping = RedcapList$mappings, 
+                            records = RedcapList$records, 
+                            project_info = RedcapList$project_information)
+      })
+    })
+    
+    unlink(rdata_file)
+    unlink(temp_dir)
+  }
+)
 
-# test_that(
-#   "Purge and restore a project from a list",
-#   {
-#     expect_no_error(purgeProject(rcon, records = TRUE))
-#     
-#     expect_no_error(restoreProject(TestingBranchingLogic, 
-#                                    rcon = rcon))
-#   }
-# )
+test_that(
+  "Preserve a project to CSV files", 
+  {
+    temp_dir <- tempdir()
+    
+    expect_true(preserveProject(rcon, 
+                                save_as = "csv", 
+                                dir = temp_dir, 
+                                dir_create = TRUE))
+    
+    csv_files <- file.path(temp_dir, 
+                           sprintf("project-%s-%s.csv", 
+                                   rcon$projectInformation()$project_id, 
+                                   c("project_information", 
+                                     "arms", 
+                                     "events", 
+                                     "meta_data", 
+                                     "mappings", 
+                                     "repeating_instruments", 
+                                     "users", 
+                                     "user_roles", 
+                                     "user_role_assignments", 
+                                     "dags", 
+                                     "dag_assignments", 
+                                     "records")))
+    
+    expect_no_error({
+      suppressWarnings({
+        off_con <- 
+          offlineConnection(meta_data = csv_files[4], 
+                            arms = csv_files[2], 
+                            events = csv_files[3],  
+                            mapping = csv_files[5], 
+                            records = csv_files[12], 
+                            project_info = csv_files[1])
+      })
+    })
+    
+    for (cf in csv_files){
+      unlink(cf)
+    }
+    unlink(temp_dir)
+  }
+)
