66118b23582458c529a39ea635e69413fb219efc
Shawn Garbett <shawn@garbett.org>
2024-01-24 17:09:16 -0600

    Improved gsub robustness #290 #291 #314

--- R/makeApiCall.R
+++ R/makeApiCall.R
@@ -263,7 +263,7 @@ makeApiCall <- function(rcon,
 as.data.frame.response <- function(x, stringsAsFactors=FALSE, na.strings = "", ...)
 {
   enc <- if(grepl("charset", x$headers[["Content-Type"]]))
-    toupper(sub(".*charset=", "", x$headers[["Content-Type"]])) else
+    toupper(sub('.*charset=([^;]+).*', '\\1', x$headers[["Content-Type"]])) else
     'ISO-8859-1' # [Default if unspecified](https://www.w3.org/International/articles/http-charset/index)
   mapped <- iconv(readBin(x$content, character()),
                   enc, 'UTF-8', '\U25a1')
