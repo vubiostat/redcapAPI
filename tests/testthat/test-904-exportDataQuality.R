2d44bc133c9f3e855a19afc9f2de65d8e9024627
Shawn Garbett <shawn@garbett.org>
2024-05-14 13:23:49 -0500

    Added name check on Data Quality returned object #321

--- tests/testthat/test-904-exportDataQuality.R
+++ tests/testthat/test-904-exportDataQuality.R
@@ -8,6 +8,17 @@ test_that("Data queries can be exported",{
   
   dq <- exportDataQuality(dqrcon, prefix)
   expect_gte(length(dq), 1)
+  
+  expect_contains(names(dq),
+    c("status_id",            "rule_id",       "pd_rule_id",
+      "non_rule",             "project_id",    "record",
+      "event_id",             "field_name",    "repeat_instrument",
+      "instance",              "status",        "exclude",
+      "query_status",         "group_id",      "assigned_username", 
+      "res_id",               "ts",            "response_requested",
+      "response",             "comment",       "username",
+      "current_query_status", "upload_doc_id", "field_comment_edited")
+  )
 })
 
 test_that(
