be766de7cff17982d5afd4cf2e8333dfe1d2119d
Shawn Garbett <shawn@garbett.org>
2024-06-27 11:36:15 -0500

    Updated tests for email_logging #390

--- tests/testthat/test-361-userStructureVersion.R
+++ tests/testthat/test-361-userStructureVersion.R
@@ -9,11 +9,34 @@ test_that("User structure contains api_modules starting at 14.0.3",
   expect_true("api_modules" %in% names(redcapUserStructure("14.0.3")))
 })
 
+test_that("User structure contains email_logging starting at 14.4.0",
+{
+  expect_false("email_logging" %in% names(redcapUserStructure("1.0.3")))
+  expect_false("email_logging" %in% names(redcapUserStructure("14.3.4")))
+  expect_true("email_logging" %in% names(redcapUserStructure("14.4.0")))
+})
+
+test_that("Prep user import pre 14.4.0 fails if given email_logging",
+{
+  local_reproducible_output(width = 200)
+  old_rcon <- rcon
+  old_rcon[['version']] <- function(){ "14.3.4" }
+
+  expect_error(prepUserImportData(User, 
+                                  rcon = old_rcon), 
+               "api_modules")
+  
+  old_rcon[['version']] <- function(){ "14.4.0" }
+  expect_no_error(prepUserImportData(User, rcon = old_rcon))
+})
+
 test_that("Prep user import pre 14.0.3 fails if given api_modules",
 {
   local_reproducible_output(width = 200)
   old_rcon <- rcon
   old_rcon[['version']] <- function(){ "14.0.2" }
+  
+  User$email_logging <- NULL
   
   expect_error(prepUserImportData(User, 
                                   rcon = old_rcon), 
@@ -22,3 +45,4 @@ test_that("Prep user import pre 14.0.3 fails if given api_modules",
   old_rcon[['version']] <- function(){ "14.0.3" }
   expect_no_error(prepUserImportData(User, rcon = old_rcon))
 })
+
