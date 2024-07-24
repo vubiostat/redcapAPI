45afce12247afcb82bd16350da140dde763e1cb6
Shawn Garbett <shawn@garbett.org>
2024-07-24 08:37:16 -0500

    The OCDP edit #397

--- R/curl.R
+++ R/curl.R
@@ -2,160 +2,178 @@
 
 .curlCompact <- function(x)
 {
-    lx <- vapply(x, length, numeric(1))
-    x[lx != 0]
+  x[vapply(x, length, numeric(1)) != 0]
 }
 
-.curlConfig <- function(..., token = NULL)
+.curlConfig <- function(...,
+                        token = NULL)
 {
-    opt <- list(...)
-    structure(list(
-        method = NULL,
-        url = NULL,
-        headers = NULL,
-        fields = NULL,
-        options = .curlCompact(opt),
-        auth_token = token,
-        output = NULL
-    ), class = "request")
+  structure(list(
+    method     = NULL,
+    url        = NULL,
+    headers    = NULL,
+    fields     = NULL,
+    options    = .curlCompact(list(...)),
+    auth_token = token,
+    output     = NULL
+  ), class = "request")
 }
 
-.curlSetConfig <- function(config, override = FALSE)
+.curlSetConfig <- function(config,
+                           override = FALSE)
 {
-    stopifnot(inherits(config, "request"))
-    old <- getOption("httr_config")
-    if(is.null(old)) old <- .curlConfig()
-    if(!override) {
-      config <- .curlRequestCombine(old, config)
-    }
-    options(httr_config = config)
-    invisible(old)
+  stopifnot(inherits(config, "request"))
+  old <- getOption("httr_config")
+  
+  if(is.null(old)) old    <- .curlConfig()
+  if(!override)    config <- .curlRequestCombine(old, config)
+
+  options(httr_config = config)
+  invisible(old)
 }
 
-.curlUploadFile <- function(path, type = NULL)
+.curlUploadFile <- function(path,
+                            type = NULL)
 {
-    stopifnot(is.character(path), length(path) == 1, file.exists(path))
-    if (is.null(type)) {
-        type <- mime::guess_type(path)
-    }
-    curl::form_file(path, type)
+  stopifnot(is.character(path), length(path) == 1, file.exists(path))
+  if (is.null(type)) type <- mime::guess_type(path)
+  curl::form_file(path, type)
 }
 
 .curlTimeout <- function(seconds)
 {
-    if (seconds < 0.001) {
-        stop("Timeout cannot be less than 1 ms", call. = FALSE)
-    }
-    .curlConfig(timeout_ms = seconds * 1000)
+  if (seconds < 0.001) stop("Timeout cannot be less than 1 ms", call. = FALSE)
+
+  .curlConfig(timeout_ms = seconds * 1000)
 }
 
-.curlContent <- function(x, type = 'text/plain', ...)
+.curlContent <- function(x, 
+                         type = 'text/plain',
+                         ...)
 {
-    stopifnot(inherits(x, "response"))
-    if (inherits(x$content, 'path')) {
-        raw <- readBin(x$content, "raw", file.info(x$content)$size)
-    } else {
-        raw <- x$content
-    }
-    if (length(raw) == 0) return("")
+  stopifnot(inherits(x, "response"))
+  raw <- if (inherits(x$content, 'path'))
+         {
+           readBin(x$content, "raw", file.info(x$content)$size)
+         } else
+         {
+           x$content
+         }
+  if (length(raw) == 0) return("")
 
-    enc <- if(grepl("charset", x$headers[["content-type"]]))
-      toupper(sub('.*charset=([^;]+).*', '\\1', x$headers[["content-type"]]))
-    else
-      'ISO-8859-1' # [Default if unspecified](https://www.w3.org/International/articles/http-charset/index)
-    x <- iconv(readBin(raw, character()), from = enc, to = 'UTF-8', '\U25a1')
-    if(grepl('\U25a1', x)) warning("Project contains invalid characters. Mapped to '\U25a1'.")
-    if(type == 'text/csv') {
-        utils::read.csv(x, ...)
-    } else if(type == 'application/json') {
-        jsonlite::fromJSON(x, simplifyVector = FALSE, ...)
-    } else {
-        x
-    }
+  enc <- if(grepl("charset", x$headers[["content-type"]]))
+         {
+           toupper(sub('.*charset=([^;]+).*', '\\1', x$headers[["content-type"]]))
+         } else
+         {
+           'ISO-8859-1' # [Default if unspecified](https://www.w3.org/International/articles/http-charset/index)
+         }
+  x <- iconv(readBin(raw, character()), from = enc, to = 'UTF-8', '\U25a1')
+  if(grepl('\U25a1', x)) warning("Project contains invalid characters. Mapped to '\U25a1'.")
+  
+  if(type == 'text/csv')
+  {
+    utils::read.csv(x, ...)
+  } else if(type == 'application/json')
+  {
+    jsonlite::fromJSON(x, simplifyVector = FALSE, ...)
+  } else
+  {
+    x
+  }
 }
 
-.curlDefaultUa <- function() {
-    # cache this?
-    versions <- c(libcurl = curl::curl_version()$version, `r-curl` = as.character(utils::packageVersion("curl")))
-    paste0(names(versions), "/", versions, collapse = " ")
+.curlDefaultUa <- function()
+{
+  # cache this?
+  versions <- c(libcurl = curl::curl_version()$version, `r-curl` = as.character(utils::packageVersion("curl")))
+  paste0(names(versions), "/", versions, collapse = " ")
 }
 
-.curlRequestCombine <- function(x, y) {
-    z <- structure(list(
-        method = NULL,
-        url = NULL,
-        headers = NULL,
-        fields = c(x$fields, y$fields),
-        options = NULL,
-        auth_token = NULL,
-        output = NULL
-    ), class = "request")
-    h <- c(x$headers, y$headers)
-    o <- c(x$options, y$options)
-    z$headers <- h[!duplicated(names(h), fromLast = TRUE)]
-    z$options <- o[!duplicated(names(o), fromLast = TRUE)]
-    for(i in c('method','url','auth_token','output')) {
-        if(!is.null(y[[i]])) {
-            z[[i]] <- y[[i]]
-        } else if(!is.null(x[[i]])) {
-            z[[i]] <- x[[i]]
-        }
+.curlRequestCombine <- function(x, 
+                                y)
+{
+  z <- structure(list(
+    method     = NULL,
+    url        = NULL,
+    headers    = NULL,
+    fields     = c(x$fields, y$fields),
+    options    = NULL,
+    auth_token = NULL,
+    output     = NULL
+  ), class = "request")
+  h <- c(x$headers, y$headers)
+  o <- c(x$options, y$options)
+  z$headers <- h[!duplicated(names(h), fromLast = TRUE)]
+  z$options <- o[!duplicated(names(o), fromLast = TRUE)]
+  for(i in c('method','url','auth_token','output'))
+  {
+    if(!is.null(y[[i]]))
+    {
+      z[[i]] <- y[[i]]
+    } else if(!is.null(x[[i]]))
+    {
+      z[[i]] <- x[[i]]
     }
-    z
+  }
+  z
 }
 
-.curlRequestDefault <- function() {
-    dr <- structure(list(
-        method = NULL,
-        url = NULL,
-        headers = c(Accept = "application/json, text/xml, application/xml, */*"),
-        fields = NULL,
-        options = list(useragent = .curlDefaultUa()),
-        auth_token = NULL,
-        output = structure(list(), class = c("write_memory", "write_function"))
-    ), class = "request")
-    .curlRequestCombine(dr, getOption("httr_config"))
+.curlRequestDefault <- function()
+{
+  dr <- structure(list(
+    method     = NULL,
+    url        = NULL,
+    headers    = c(Accept = "application/json, text/xml, application/xml, */*"),
+    fields     = NULL,
+    options    = list(useragent = .curlDefaultUa()),
+    auth_token = NULL,
+    output     = structure(list(), class = c("write_memory", "write_function"))
+  ), class = "request")
+  .curlRequestCombine(dr, getOption("httr_config"))
 }
 
-.curlPost <- function(url, body, config = list()) {
-    h <- curl::new_handle()
-    body <- .curlCompact(body)
-    nms <- names(body)
-    if(is.null(nms) || any(is.na(nms) | nms == '')) {
-        stop("All components of body must be named", call. = FALSE)
-    }
-    flds <- lapply(body, function(x) as.character(x))
+.curlPost <- function(url, 
+                      body,
+                      config = list())
+{
+  h    <- curl::new_handle()
+  body <- .curlCompact(body)
+  nms  <- names(body)
+  if(is.null(nms) || any(is.na(nms) | nms == '')) 
+    stop("All components of body must be named", call. = FALSE)
 
-    req <- structure(list(
-        method = "POST",
-        url = url,
-        headers = config$headers,
-        fields = c(flds, config$fields),
-        options = config$options,
-        auth_token = config$auth_token,
-        output = config$output
-    ), class = "request")
-    req <- .curlRequestCombine(.curlRequestDefault(), req)
-    req$options$post <- TRUE
+  flds <- lapply(body, function(x) as.character(x))
 
-    curl::handle_setopt(h, .list = req$options)
-    if (!is.null(req$fields)) {
-        curl::handle_setform(h, .list = req$fields)
-    }
-    curl::handle_setheaders(h, .list = req$headers)
-    on.exit(curl::handle_reset(h), add = TRUE)
+  req <- structure(list(
+    method     = "POST",
+    url        = url,
+    headers    = config$headers,
+    fields     = c(flds, config$fields),
+    options    = config$options,
+    auth_token = config$auth_token,
+    output     = config$output
+  ), class     = "request")
+  req <- .curlRequestCombine(.curlRequestDefault(), req)
+  req$options$post <- TRUE
 
-    resp <- curl::curl_fetch_memory(req$url, h)
-    rh <- curl::parse_headers_list(resp$headers)
-    structure(list(
-        url = resp$url,
-        status_code = resp$status_code,
-        headers = rh,
-        all_headers = resp$headers,
-        cookies = curl::handle_cookies(h),
-        content = resp$content,
-        times = resp$times,
-        request = req,
-        handle = h
-    ), class = "response")
+  curl::handle_setopt(h, .list = req$options)
+  if (!is.null(req$fields)) curl::handle_setform(h, .list = req$fields)
+
+  curl::handle_setheaders(h, .list = req$headers)
+  on.exit(curl::handle_reset(h), add = TRUE)
+
+  resp <- curl::curl_fetch_memory(req$url, h)
+  rh   <- curl::parse_headers_list(resp$headers)
+  structure(list(
+    url         = resp$url,
+    status_code = resp$status_code,
+    headers     = rh,
+    all_headers = resp$headers,
+    cookies     = curl::handle_cookies(h),
+    content     = resp$content,
+    times       = resp$times,
+    request     = req,
+    handle      = h
+  ), class = "response")
 }
