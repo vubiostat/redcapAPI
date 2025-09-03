8569b99685a240f8adf10afb68014919d8f42638
Shawn Garbett <shawn@garbett.org>
2025-09-03 12:10:09 -0500

    Refined logging draft for splunk #458

--- R/logging.R
+++ R/logging.R
@@ -9,7 +9,8 @@ current_log_level <- function()
 
   if(!level %in% names(log_levels))
   {
-    warning(paste("Undefined redcapAPI_log_level", level, "\nDefaulting to INFO."))
+    warning(paste("Undefined redcapAPI_log_level", level,
+                  "\nDefaulting to INFO."))
     level <- 'INFO'
   }
 
@@ -24,30 +25,42 @@ current_logger <- function(...)
   getOption('redcapAPI_logger', default_logger)(...)
 
 #' @importFrom rjson toJSON
-splunk_request = function(token, url, packet)
+#' @impmorFrom curl new_handle handle_setheaders handle_setopt handle_reset curl_fetch_memory
+.splunkPost <- function(token, url, body)
 {
-  config <- list(url = url,
-                 Authorization = paste("Splunk", token))
-  response <- .curlPost(body = toJSON(packet), config = config)
-  if(!response$status_code %in% success_status_codes)
-    warning(paste("Splunk logging call failed with ", response$status_code))
-
-  invisible(response)
+  h    <- curl::new_handle()
+  handle_setheaders(h, .list=c(
+    "Authorization" = paste("Splunk", token),
+    "Content-Type"  = "application/json"
+  ))
+  handle_setopt(h, postfields = toJSON(body))
+  on.exit(curl::handle_reset(h), add = TRUE)
+  resp <- curl_fetch_memory(url, handle=h)
+  if(resp$status_code != 200L)
+    warning(paste("Splunk logging call failed", response$status_code))
+  invisible(resp)
 }
 
 # Creates a logger function for SPLUNK
 # url <- "https://splunkinput.app.vumc.org/services/collector"
-splunk_logger_FUN <- function(token, url, project)
+splunk_logger_FUN <- function(
+    token=Sys.getenv('SPLUNK_TOKEN'),
+    url=Sys.getenv("SPLUNK_URL"),
+    project=basename(getwd()))
 {
+  if(token == '') stop("Splunk token not set when setting logging function.")
+  if(url == '')   stop("Splunk url not set when setting logging function.")
+
   function(level, ...)
   {
     if(log_levels[toupper(level)] <  current_log_level()) return(invisible(NULL))
 
-    packet              <- list(...)
-    packet$time         <- Sys.time()
-    packet$type         <- level  # Shouldn't this be "level"
-    packet$project_type <- project
+    browser()
+    packet         <- list(...)
+    packet$time    <- Sys.time()
+    packet$level   <- level
+    packet$project <- project
 
-    splunk_request(token, url, packet)
+    .splunkPost(token, url, packet)
   }
 }
