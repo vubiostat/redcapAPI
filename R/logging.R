bbbb062007012af7b1b0fa4b8fe30ef6a6e63e45
Shawn Garbett <shawn@garbett.org>
2025-09-03 12:28:20 -0500

    More draft revision of logging #458

--- R/logging.R
+++ R/logging.R
@@ -35,21 +35,43 @@ current_logger <- function(...)
   ))
   handle_setopt(h, postfields = toJSON(body))
   on.exit(curl::handle_reset(h), add = TRUE)
-  resp <- curl_fetch_memory(url, handle=h)
+  resp <- tryCatch(
+  {
+    curl_fetch_memory(url, handle=h)
+  },
+  error=function(e)
+  {
+    if(grepl("Timeout was reached", e$message))
+    {
+      warning("Due to timeout disabling logging.")
+      options(redcapAPI_logger='')
+      structure(
+        list(
+          status_code = 408L,
+          content = charToRaw(e$message),
+          headers = list('content-type' = "text/plain; charset=utf-8")
+        ),
+        class="response")
+    } else
+    {
+      stop(e)
+    }
+  })
   if(resp$status_code != 200L)
     warning(paste("Splunk logging call failed", response$status_code))
   invisible(resp)
 }
 
 # Creates a logger function for SPLUNK
-# url <- "https://splunkinput.app.vumc.org/services/collector"
+# Our Test URL "https://splunkinput.app.vumc.org/services/collector"
 splunk_logger_FUN <- function(
-    token=Sys.getenv('SPLUNK_TOKEN'),
-    url=Sys.getenv("SPLUNK_URL"),
-    project=basename(getwd()))
+  token   = Sys.getenv('SPLUNK_TOKEN'),
+  url     = Sys.getenv('SPLUNK_URL'),
+  project = Sys.getenv('SPLUNK_PROJECT'))
 {
-  if(token == '') stop("Splunk token not set when setting logging function.")
-  if(url == '')   stop("Splunk url not set when setting logging function.")
+  if(token   == '') stop("Splunk token not set when creating logging function.")
+  if(url     == '') stop("Splunk url not set when creating logging function.")
+  if(project == '') project <- basename(getwd())
 
   function(level, ...)
   {
@@ -63,4 +85,35 @@ splunk_logger_FUN <- function(
 
     .splunkPost(token, url, packet)
   }
+}
+
+.find_first_exported_call_from_package <- function(pkg)
+{
+  # Get the list of exported objects (symbols) from the package
+  exported <- getNamespaceExports(pkg)
+  ns_env   <- asNamespace(pkg)
+
+  # Loop over call stack
+  for (i in seq_along(sys.calls()))
+  {
+    call <- sys.call(i)
+    fun  <- sys.function(i)
+
+    # Try to find the name of the function if it's named
+    if (is.symbol(call[[1]]) || is.call(call[[1]]))
+    {
+      fun_name <- deparse(call[[1]])
+
+      # Try to find the object in the package namespace
+      if (exists(fun_name, envir = ns_env, inherits = FALSE))
+      {
+        exported_fun <- get(fun_name, envir = ns_env)
+
+        # Check if it's exported and ident
+        if (fun_name %in% exported && identical(fun, exported_fun)) return(call)
+      }
+    }
+  }
+
+  return(NULL)
 }
