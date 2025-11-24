b743eca811fba3fc1689660b3f8b8c9902e0fba8
Shawn Garbett <shawn@garbett.org>
2025-11-24 13:40:13 -0600

    Clearer importUsers error message #471

--- R/importUsers.R
+++ R/importUsers.R
@@ -10,76 +10,80 @@ importUsers <- function(rcon, data, ...){
 #' @order 5
 #' @export
 
-importUsers.redcapApiConnection <- function(rcon, 
+importUsers.redcapApiConnection <- function(rcon,
                                             data,
-                                            consolidate = TRUE, 
+                                            consolidate = TRUE,
                                             ...)
 {
   ###################################################################
   # Argument Validation                                          ####
-  
+
   coll <- checkmate::makeAssertCollection()
-  
-  checkmate::assert_class(x = rcon, 
-                          classes = "redcapApiConnection", 
+
+  checkmate::assert_class(x = rcon,
+                          classes = "redcapApiConnection",
                           add = coll)
-  
-  checkmate::assert_data_frame(x = data, 
-                               col.names = "named", 
+
+  checkmate::assert_data_frame(x = data,
+                               col.names = "named",
                                add = coll)
-  
-  checkmate::assert_logical(x = consolidate, 
-                            len = 1, 
-                            null.ok = FALSE, 
+
+  checkmate::assert_logical(x = consolidate,
+                            len = 1,
+                            null.ok = FALSE,
                             add = coll)
 
   checkmate::reportAssertions(coll)
-  
+
   form_names <- rcon$instruments()$instrument_name
   form_access_names <- sprintf("%s_form_access", form_names)
   form_export_names <- sprintf("%s_export_access", form_names)
-  
-  checkmate::assert_subset(x = names(data), 
-                           choices = c(names(redcapUserStructure(rcon$version())), 
-                                       form_access_names, 
-                                       form_export_names, 
-                                       "data_export"), 
-                           add = coll)
-  
+
+  invalid_col_names <-
+    names(data)[!names(data) %in%
+                c(names(redcapUserStructure(rcon$version())),
+                  form_access_names,
+                  form_export_names,
+                  "data_export")]
+  checkmate::assert_vector(x=invalid_col_names,
+                           len=0,
+                           add = coll,
+                           .var.name=paste("names(data) has invalid columns", paste(invalid_col_names, collapse=", ")))
+
   checkmate::reportAssertions(coll)
-  
+
   data <- prepUserImportData(data,
                              rcon = rcon,
                              consolidate = consolidate)
-  
-  
+
+
   ###################################################################
   # Check for Users Assigned to User Role                        ####
-  
+
   OrigUserRoleAssign <- rcon$user_role_assignment()
 
   user_conflict_exists <- .importUsers_detectUserRoleConflict(rcon, data)
-  
+
   ###################################################################
   # Build the body list                                          ####
-  
-  body <- list(content = "user", 
-               format = "csv", 
-               returnFormat = "csv", 
+
+  body <- list(content = "user",
+               format = "csv",
+               returnFormat = "csv",
                data = writeDataForImport(data))
 
   ###################################################################
   # Make the API Call                                            ####
   rcon$flush_users()
   response <- makeApiCall(rcon, body, ...)
-  
+
   ###################################################################
   # Restore and refresh                                          ####
   if (user_conflict_exists){
-    importUserRoleAssignments(rcon, 
+    importUserRoleAssignments(rcon,
                               data = OrigUserRoleAssign[1:2])
   }
-  
+
   invisible(as.character(response))
 }
 
@@ -89,20 +93,20 @@ importUsers.redcapApiConnection <- function(rcon,
 
 .importUsers_detectUserRoleConflict <- function(rcon, data){
   UsersAssignedRoles <- rcon$user_role_assignment()
-  UsersAssignedRoles <- 
+  UsersAssignedRoles <-
     UsersAssignedRoles[!is.na(UsersAssignedRoles$unique_role_name), ]
-  UsersWithConflict <- 
+  UsersWithConflict <-
     UsersAssignedRoles[UsersAssignedRoles$username %in% data$username, ]
-  
+
   user_conflict_exists <- nrow(UsersWithConflict) > 0
-  
+
   if (user_conflict_exists){
-    UsersWithConflict$unique_role_name <- rep(NA_character_, 
+    UsersWithConflict$unique_role_name <- rep(NA_character_,
                                               nrow(UsersWithConflict))
-    
-    importUserRoleAssignments(rcon, 
+
+    importUserRoleAssignments(rcon,
                               data = UsersWithConflict[1:2])
   }
-  
+
   user_conflict_exists
 }
