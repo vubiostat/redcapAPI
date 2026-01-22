b8577fbc110c871a8b4b00d58eaa3a7e72074de6
Shawn Garbett <shawn@garbett.org>
2026-01-22 10:36:18 -0600

    Adding empty frame contanst for file repository changes #505

--- R/constants.R
+++ R/constants.R
@@ -8,19 +8,21 @@
 # This is used for File Repository methods. Some calls result in a
 # result of an empty string or an error. At times we prefer to return
 # the empty frame to maintain consistency in outputs with recursive calls
-FILE_REPOSITORY_EMPTY_FRAME <- 
-  data.frame(folder_id = numeric(0), 
-             doc_id = numeric(0), 
-             name = character(0), 
+FILE_REPOSITORY_EMPTY_FRAME <-
+  data.frame(folder_id = numeric(0),
+             doc_id = numeric(0),
+             name = character(0),
+             role = character(0),
+             dag = character(0),
              parent_folder = numeric(0),
              stringsAsFactors = FALSE)
 
 # This is used for cases when an import/export/delete to the file repository
 # results in no changes and an empty frame is needed for the return
 
-FILE_IMPORT_EXPORT_EMPTY_FRAME <- 
-  data.frame(directory = character(0), 
-             filename = character(0), 
+FILE_IMPORT_EXPORT_EMPTY_FRAME <-
+  data.frame(directory = character(0),
+             filename = character(0),
              stringsAsFactors = FALSE)
 
 
@@ -32,12 +34,12 @@ FILE_IMPORT_EXPORT_EMPTY_FRAME <-
 # use sub(REGEX_CHECKBOX_FIELD_NAME, "\\2", x, perl = TRUE) to get the option
 # Using regex to parse checkbox names has some limitations and at some point
 # we may need to consider another strategy. REDCap permits any characters to
-# be used in coding checkboxes, but when converting them to field names, 
-# converts non alphnumeric characters to an underscore. 
+# be used in coding checkboxes, but when converting them to field names,
+# converts non alphnumeric characters to an underscore.
 # some examples -4, Option  ----> checkbox____4
 #               k>=9, K >=9 ----> checkbox___k__9
 #               a___b       ----> checkbox___a___b
-# 
+#
 # Explanation : https://stackoverflow.com/a/76020417/1017276
 # ^(.*?)___ : match anything at beginning non-greedily followed by ___ into 1st group
 #       ___ : after that, find the first case of ___ (the REDCap UI will not permit the user to use more than one consecutive _ in field names)
@@ -74,51 +76,51 @@ REGEX_FORM_NAME <- "(^[a-z]$|^[a-z][a-z,0-9]$|^[a-z](?!.*__.*)[a-z,0-9,_]+[a-z,0
 
 # REGEX_MULT_CHOICE_STRICT - matches acceptable formats for multiple choice options
 # It's a good idea to trim whitespace before using this
-# This 'STRICT' regex requires that definitions follow the pattern of 
-# [code1], [label1] | [code2], [label2]. It turns out that there are ways 
-# that users can define multiple choice fields using only the labels 
-# (via the metadata CSV import and the API will actually allow it, too). 
+# This 'STRICT' regex requires that definitions follow the pattern of
+# [code1], [label1] | [code2], [label2]. It turns out that there are ways
+# that users can define multiple choice fields using only the labels
+# (via the metadata CSV import and the API will actually allow it, too).
 # We have decided that we do not want to permit this through importMetaData
 # so a separate regex is needed for the import than we use for the export.
 # See issue 145.
 # Explanation - this one makes my head swim a bit, but I'll do my best. (BN)
 #                       ^ : Start of string
-#                 [^\\|]+ : Any number of character, but the sequence may not 
+#                 [^\\|]+ : Any number of character, but the sequence may not
 #                           start with a pipe (this is the first code)
 #                       , : literal comma
-#                 [^\\|]* : any number of characters, but the sequence may not 
+#                 [^\\|]* : any number of characters, but the sequence may not
 #                           start with a pipe (this is the first label)
-#                           it is a lazy match, meaning it will stop the first 
+#                           it is a lazy match, meaning it will stop the first
 #                           time it hits a pipe
 # (?:\\|[^\\|]+,[^\\|]*)* : this is the workhorse, it's looking for a repeating pattern
-#                         : of pipe, characters, comma, character, pipe with 
+#                         : of pipe, characters, comma, character, pipe with
 #                         : a terminating sequence of pipe, characters, comma, characters.
 #                         : That is, the last in the sequence does not end with a pipe
 #                       $ : end of string
 
 REGEX_MULT_CHOICE_STRICT <- "^[^\\|]+,[^\\|]*(?:\\|[^\\|]+,[^\\|]*)*$"
 
-# REGEX_MULT_CHOICE - matches acceptable formats for multiple choice options, 
-# to include formats that use only the label. See Issue 145. 
-# It's a good idea to trim whitespace before using this. 
+# REGEX_MULT_CHOICE - matches acceptable formats for multiple choice options,
+# to include formats that use only the label. See Issue 145.
+# It's a good idea to trim whitespace before using this.
 # Explanation - uses the same pattern as REGEX_MULT_CHOICE_STRICT and also includes the following
 #               for matching shorthand multiple choice fields.
 #                       ^ : Start of string
 #           (?:[^|,]+\|)+ : Look for a repeating pattern of character, pipe, character, where
-#                           the last in the sequence does not end with a pipe and characters 
+#                           the last in the sequence does not end with a pipe and characters
 #                           do not include commas
-#                  [^|,]+ : any number of characters, but the sequence may not 
+#                  [^|,]+ : any number of characters, but the sequence may not
 #                           include a pipe or comma
 #                       $ : end of string
 
 REGEX_MULT_CHOICE <- "^(^[^\\|]+,[^\\|]*(?:\\|[^\\|]+,[^\\|]*)*$|^(?:[^|,]+\\|)+[^|,]+$)$"
-                     
+
 # REGEX_SLIDER - matches acceptable definition of slider bar settings
 # Specifically, low point | midpoint | high point
-# Any of the three values may be missing, but the two pipes must be 
+# Any of the three values may be missing, but the two pipes must be
 # present.
 # This is fairly permissive, but it seems to match REDCap behaviors
-# REDCap does not actually restrict what characters can go between the pipes, 
+# REDCap does not actually restrict what characters can go between the pipes,
 # although it produces weird results when you include a pipe within the labels.
 # Example:  this | and|or | that will produce labels "this", "and", and "or | that" in the UI.
 # Explanation
